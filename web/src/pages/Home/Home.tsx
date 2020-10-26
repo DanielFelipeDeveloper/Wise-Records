504964b5e7662d5deff5a18b83525f6021fe17af
DanielDeveloper <danielfelipedeveloper@gmail.com>
2020-10-26 15:59:50 -0400

    Feat: Cards

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -1,10 +1,119 @@
 import React from 'react';
 
+import { ImPlay3 } from 'react-icons/im'
+import { FaShoppingCart } from 'react-icons/fa'
+
 import Header from '../../components/Header/Header';
+
+import './styles.css'
 
 const Home: React.FC = () => {
   return (
-    <Header />
+    <>
+      <Header />
+
+      <div className="cards-container">
+        <div className="card">
+          <div className="card-img">
+            <img
+              src="https://cdn.airbit.com/artwork/2f20a1eba7d27ae74f531de136956368@300x.jpg"
+              alt="card"
+            />
+            <div className="btn-player">
+              <button><ImPlay3 fontSize={25} color="#fff" /></button>
+            </div>
+          </div>
+          <div className="card-description">
+            <div>
+              <span>Nome do beat</span>
+              <p>Trap</p>
+              <p>140 BPM</p>
+            </div>
+            <div>
+              <button>
+                <FaShoppingCart color="#fff"fontSize={16}/>
+                <span>R$ 80.00</span>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-img">
+            <img
+              src="https://cdn.airbit.com/artwork/2f20a1eba7d27ae74f531de136956368@300x.jpg"
+              alt="card"
+            />
+            <div className="btn-player">
+              <button><ImPlay3 fontSize={25} color="#fff" /></button>
+            </div>
+          </div>
+          <div className="card-description">
+            <div>
+              <span>Nome do beat</span>
+              <p>Trap</p>
+              <p>140 BPM</p>
+            </div>
+            <div>
+              <button>
+                <FaShoppingCart color="#fff"fontSize={16}/>
+                <span>R$ 80.00</span>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-img">
+            <img
+              src="https://cdn.airbit.com/artwork/2f20a1eba7d27ae74f531de136956368@300x.jpg"
+              alt="card"
+            />
+            <div className="btn-player">
+              <button><ImPlay3 fontSize={25} color="#fff" /></button>
+            </div>
+          </div>
+          <div className="card-description">
+            <div>
+              <span>Nome do beat</span>
+              <p>Trap</p>
+              <p>140 BPM</p>
+            </div>
+            <div>
+              <button>
+                <FaShoppingCart color="#fff"fontSize={16}/>
+                <span>R$ 80.00</span>
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-img">
+            <img
+              src="https://cdn.airbit.com/artwork/2f20a1eba7d27ae74f531de136956368@300x.jpg"
+              alt="card"
+            />
+            <div className="btn-player">
+              <button><ImPlay3 fontSize={25} color="#fff" /></button>
+            </div>
+          </div>
+          <div className="card-description">
+            <div>
+              <span>Nome do beat</span>
+              <p>Trap</p>
+              <p>140 BPM</p>
+            </div>
+            <div>
+              <button>
+                <FaShoppingCart color="#fff"fontSize={16}/>
+                <span>R$ 80.00</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
