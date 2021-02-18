69c7cd20b6006d6fbf3b681ddcab5424a614a802
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-02-18 17:44:41 -0400

    feat: login modal validation

--- web/src/components/LoginModal/index.tsx
+++ web/src/components/LoginModal/index.tsx
@@ -3,8 +3,10 @@ import React, { useCallback, useRef } from 'react';
 import { FiFacebook, FiLock, FiUser } from 'react-icons/fi';
 import { MdClose } from 'react-icons/md';
 import { AiFillGoogleCircle } from 'react-icons/ai';
+import * as Yup from 'yup';
 
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
 import {
   Modal,
@@ -17,6 +19,7 @@ import {
   CloseButton,
 } from './styles';
 import Input from '../Input';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 interface ModalProps {
   isOpen: boolean;
@@ -25,6 +28,7 @@ interface ModalProps {
 
 const LoginModal: React.FC<ModalProps> = ({ isOpen, onClose }: ModalProps) => {
   const overlayRef = useRef(null);
+  const formRef = useRef<FormHandles>(null);
 
   const handleOverlayClick = useCallback(
     (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
@@ -35,8 +39,23 @@ const LoginModal: React.FC<ModalProps> = ({ isOpen, onClose }: ModalProps) => {
     [onClose],
   );
 
-  const handleSubmit = useCallback((data: object) => {
-    console.log(data); //eslint-disable-line
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string().required().email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'Senha precisa ter no mínimo 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = await getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
   }, []);
 
   return isOpen ? (
@@ -47,7 +66,7 @@ const LoginModal: React.FC<ModalProps> = ({ isOpen, onClose }: ModalProps) => {
             <MdClose color="#fff" fontSize={17} onClick={onClose} />
           </CloseButton>
           <h1>Login</h1>
-          <Form onSubmit={handleSubmit}>
+          <Form ref={formRef} onSubmit={handleSubmit}>
             <Input name="email" icon={FiUser} placeholder="Email" />
             <Input
               name="password"
