bf2cc327b120af9864c9fa03a80d295ab131f72e
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-02-18 20:01:02 -0400

    feat: habilite cors

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Header from '../../components/Header';
 import Card from '../../components/Card';
 import FooterPlayer from '../../components/FooterPlayer';
 
+import api from '../../services/api';
+
 import { Cards, Title } from './styles';
 
+interface Beats {
+  image: string;
+  name: string;
+  style: string;
+  bpm: string;
+  price: number;
+}
+
 const Home: React.FC = () => {
+  const [beats, setBeats] = useState<Beats[]>();
+
+  const getBeats = async () => {
+    try {
+      const response = await api.get('/beats');
+
+      setBeats(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getBeats();
+  }, []);
+
+  console.log(beats);
+
   return (
     <>
       <Header />
